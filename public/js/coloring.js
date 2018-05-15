afaf6cc9a9e5cdb107cea6c0e76a010a1da39166
Robert Williams <robert.williams@mutualmobile.com>
2018-05-15 15:20:54 -0500

    fixes for ios safari.

--- public/js/coloring.js
+++ public/js/coloring.js
@@ -265,13 +265,11 @@ function generateSvg() {
 
 function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
 
     element.style.display = 'none';
     document.body.appendChild(element);
-
     element.click();
-
     document.body.removeChild(element);
 }
